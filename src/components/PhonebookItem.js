29aca4cc6bf08e47970af336859d713bb04351d0
aryajava <muhammad.aryajava@gmail.com>
2024-11-17 15:19:52 +0700

    feat: add avatar upload functionality with alert handling in PhonebookItem component

--- src/components/PhonebookItem.js
+++ src/components/PhonebookItem.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPenToSquare, faTrash, faSave } from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { request, getBaseURL } from './PhonebookBox';
 
 export const PhonebookItem = (props) => {
@@ -8,6 +8,9 @@ export const PhonebookItem = (props) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedName, setEditedName] = useState(name);
   const [editedPhone, setEditedPhone] = useState(phone);
+  const fileInputRef = useRef(null);
+  const [alertMessage, setAlertMessage] = useState('');
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleEditClick = () => {
     setIsEditing(true);
@@ -26,15 +29,57 @@ export const PhonebookItem = (props) => {
     }
   };
 
+  const handleImageClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const closeAlert = () => {
+    setShowAlert(false);
+  };
+
+  const handleFileChange = async (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('avatar', file);
+
+      try {
+        const response = await request.put(`${id}/avatar`, formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+        updatePhonebookItem(id, response.data);
+      } catch (error) {
+        console.error('Error uploading avatar:', error.code);
+        setAlertMessage(error.response.data.error + '!');
+        setShowAlert(true);
+      }
+    }
+  };
+
   const avatarUrl = `${getBaseURL()}/images/${id}/${avatar}`;
 
   return (
     <div className='col-xl-3 col-md-4 col-12'>
       <div className='card'>
+        {showAlert && (
+          <div className='alert mb-3' id='alert' role='alert'>
+            <button className='close-btn' onClick={closeAlert}>x</button>
+            <p id='alertMessage'>{alertMessage}</p>
+          </div>
+        )}
         <div className='card-body'>
           <div className='row'>
             <div className='col-5 pr-3 justify-content-center'>
-              <img src={avatarUrl} alt={name} />
+              <img src={avatarUrl} alt={name} onClick={handleImageClick} style={{ cursor: 'pointer' }} />
+              <input
+                type='file'
+                ref={fileInputRef}
+                style={{ position: 'absolute', width: '1px', height: '1px', opacity: 0, overflow: 'hidden', border: 0, padding: 0, margin: '-1px' }}
+                aria-hidden='true'
+                onChange={handleFileChange}
+              />
             </div>
             <div className='col-7'>
               {isEditing ? (
