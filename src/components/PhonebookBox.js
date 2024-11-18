63e35145f0753fcfebd6e63d99897ca7f540aae5
aryajava <muhammad.aryajava@gmail.com>
2024-11-18 11:37:48 +0700

    feat: implement infinite scrolling in PhonebookBox component; add lodash for throttling and enhance data fetching logic

--- src/components/PhonebookBox.js
+++ src/components/PhonebookBox.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { throttle } from 'lodash';
 import { PhonebookList } from './PhonebookList';
 import { PhonebookTopBar } from './PhonebookTopBar';
 import { PhonebookDelete } from './PhonebookDelete';
@@ -19,19 +20,53 @@ export const PhonebookBox = () => {
   const [phonebookItems, setPhonebookItems] = useState([]);
   const [itemToDelete, setItemToDelete] = useState(null);
   const [isDeleteModalVisible, setIsDeleteModalVisible] = useState(false);
+  const [page, setPage] = useState(1);
+  const [isFetching, setIsFetching] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  const observer = useRef();
 
-  const fetchData = async () => {
+  const fetchData = async (page) => {
+    setIsFetching(true);
     try {
-      const response = await request.get();
-      setPhonebookItems(response.data.phonebooks);
+      const response = await request.get(`?page=${page}`);
+      setPhonebookItems((prevItems) => {
+        const newItems = response.data.phonebooks.filter(
+          (newItem) => !prevItems.some((item) => item.id === newItem.id)
+        );
+        return [...prevItems, ...newItems];
+      });
+      setHasMore(response.data.phonebooks.length > 0);
     } catch (error) {
       console.error(error.code);
     }
-  }
+    setIsFetching(false);
+  };
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    fetchData(page);
+  }, [page]);
+
+  const lastPhonebookElementRef = useRef();
+
+  useEffect(() => {
+    if (isFetching) return;
+    if (observer.current) observer.current.disconnect();
+
+    const handleObserver = throttle((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    }, 200); // 200ms throttle delay
+
+    observer.current = new IntersectionObserver(handleObserver);
+    if (lastPhonebookElementRef.current) {
+      observer.current.observe(lastPhonebookElementRef.current);
+    }
+
+    return () => {
+      if (observer.current) observer.current.disconnect();
+    };
+  }, [isFetching, hasMore]);
 
   const showDeleteModal = (item) => {
     setItemToDelete(item);
@@ -62,6 +97,8 @@ export const PhonebookBox = () => {
         removePhonebookItem={removePhonebookItem}
         showDeleteModal={showDeleteModal}
       />
+      {isFetching}
+      <div ref={lastPhonebookElementRef}></div>
       {isDeleteModalVisible && itemToDelete && (
         <PhonebookDelete
           id={itemToDelete.id}
